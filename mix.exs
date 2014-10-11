769ebc3c1f0e5612639084ed128bb6dc6f8c9f8b
BennyHallett <benny.hallett@gmail.com>
2014-10-12 02:43:14 +1100

    Set version

--- mix.exs
+++ mix.exs
@@ -3,27 +3,15 @@ defmodule Anubis.Mixfile do
 
   def project do
     [app: :anubis,
-     version: "0.0.1",
+     version: "0.1.0",
      elixir: "~> 1.0.0",
      deps: deps]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type `mix help compile.app` for more information
   def application do
     [applications: [:logger]]
   end
 
-  # Dependencies can be hex.pm packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1"}
-  #
-  # Type `mix help deps` for more examples and options
   defp deps do
     []
   end
