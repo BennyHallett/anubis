a94a05358e9018bcadecee71525278cbaccf9823
BennyHallett <benny.hallett@gmail.com>
2014-10-31 08:00:16 +1100

    UPdated mix file

--- mix.exs
+++ mix.exs
@@ -3,8 +3,13 @@ defmodule Anubis.Mixfile do
 
   def project do
     [app: :anubis,
-     version: "0.2.0",
+     version: "0.3.0",
      elixir: "~> 1.0.0",
+     package: package,
+     docs: [readme: true, main: "README.md"],
+     description: """
+      Anubis is a framework for building command line applications.
+     """,
      deps: deps]
   end
 
@@ -14,5 +19,13 @@ defmodule Anubis.Mixfile do
 
   defp deps do
     []
+  end
+
+  defp package do
+    %{
+      licenses: ["MIT"],
+      contributors: ["Benny Hallett"],
+      links: %{ "Github" => "https://github.com/bennyhallett/anubis" }
+    }
   end
 end
